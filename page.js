b72ae55806cd8ccf3bc65d67a3964e204b2678ff
“PeggyTsai” <pinklover1671291991@gmail.com>
2022-01-05 19:42:01 +0800

    新增分頁css樣式內容

--- page.js
+++ page.js
@@ -1,5 +1,25 @@
+const id = location.href.split("=")[1];
 
+const attractionContent = document.querySelector('.pageContent');
 
+function getAttractionContent(){
+    const id = location.href.split("=")[1];
+    axios.get(`https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24top=30&%24format=JSON&$filter=contains(ScenicSpotID,'${id}')`,
+    {
+        headers: getAuthorizationHeader()
+      })
+    .then(
+        function(response){
+            let contentData = response.data[0];
+            console.log(contentData);
+            document.querySelector(".js-title").textContent = contentData.ScenicSpotName;
+            document.querySelector(".js-img").setAttribute("src", contentData.Picture.PictureUrl1);
+            document.querySelector(".js-img").setAttribute("alt", contentData.Picture.PictureDescription1);
+            document.querySelector(".js-description").textContent = contentData.DescriptionDetail;
+
+        })
+}
+getAttractionContent();
 
 
 
