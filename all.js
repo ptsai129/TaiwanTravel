31e40ebb4c61e7af9c11f89e13ed397619a26fa3
“PeggyTsai” <pinklover1671291991@gmail.com>
2022-01-05 13:59:02 +0800

    修改縣市篩選

--- all.js
+++ all.js
@@ -137,29 +137,28 @@ function getAuthorizationHeader() {
 function init(){
   getAttractions();
 }
+init();
 
-//縣市篩選
-const selectCity = document.querySelector(".areaList");
-selectCity.addEventListener('change', function(e){
-  let city = e.target.value;
+//預設狀態 顯示所有旅遊景點資料
+function getAttractions(){
   axios.get(
-    `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/${city}?%24top=300&%24format=JSON`,
+    'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24top=30&%24format=JSON',
     {
        headers: getAuthorizationHeader()
     }
   )
   .then(function (response) {
-   let cityAttractionData = response.data;
+   let attractionData = response.data;
    let str ="";
-   cityAttractionData.forEach(function(item){
-    if(item.Picture.PictureUrl1 == undefined || item.City == undefined|| item.ScenicSpotName ==undefined){
+   attractionData.forEach(function(item){
+    if(item.Picture.PictureUrl1 == undefined   || item.Class1 == undefined|| item.ScenicSpotName ==undefined){
       return;
     }
      str+=`<li>
      <div class="attractionList-item">
        <img class="attractionList-img" src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}">
        <div class="attractionList-label">
-         <h3>${item.City}</h3>
+         <h3>${item.Class1}</h3>
        </div>
        <h4>${item.ScenicSpotName}</h4>
      </div>
@@ -170,97 +169,155 @@ selectCity.addEventListener('change', function(e){
   .catch(function (error) {
    console.log(error);
   }); 
+  }
   
-})
 
-
-//撈出所有旅遊景點資料
-function getAttractions(){
-axios.get(
-  'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24top=30&%24format=JSON',
-  {
-     headers: getAuthorizationHeader()
-  }
-)
-.then(function (response) {
- let attractionData = response.data;
- let str ="";
- attractionData.forEach(function(item){
-  if(item.Picture.PictureUrl1 == undefined  || item.Class1 == undefined|| item.ScenicSpotName ==undefined){
-    return;
-  }
-   str+=`<li>
-   <div class="attractionList-item">
-     <img class="attractionList-img" src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}">
-     <div class="attractionList-label">
-       <h3>${item.Class1}</h3>
-     </div>
-     <h4>${item.ScenicSpotName}</h4>
-   </div>
- </li>`
- })
- document.querySelector(".attractionList").innerHTML = str;
-})
-.catch(function (error) {
- console.log(error);
-}); 
-}
-
-//篩選出有哪些旅遊類別
-let tourTypes = {};
-function getTourType(){
-  axios.get(
-    `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24format=JSON`,
-    {
-      headers: getAuthorizationHeader()
-    }
-  ).then(function (response){
-   const thisData = response.data;
-   thisData.forEach(function (item){
-     if(tourTypes[item.Class1] == undefined){
-       tourTypes[item.Class1]=1;
-     }else{
-       tourTypes[item.Class1] +=1;
-     }
-   })
-   console.log(tourTypes);
-  })
-}
-//getTourType();
+//選擇縣市後再點擊按鈕取得資料
 
 const selectGroup = document.querySelector('.selectGroup');
 const selectGroupMobile = document.querySelector('.selectGroup-mobile');
-
-selectGroup.addEventListener('click', function(e){
-  if(e.target.nodeName != 'A'){
-    return;
-  }else{
-    console.log(e.target.getAttribute("data-type") );
-    let selectType = e.target.getAttribute("data-type");
-    axios.get(`https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24filter=contains(Class1,'${selectType}')&%24format=JSON`, {
-      headers:getAuthorizationHeader()
-      }
-    ).then(function(response){
-      let selectData = response.data;
-      let str ="";
-      selectData.forEach(function(item){
-        if(item.Picture.PictureUrl1 == undefined  || item.Class1 == undefined|| item.ScenicSpotName ==undefined){
+const selectCity = document.querySelector(".areaList");
+selectGroup.addEventListener('click',function(e){
+  if(e.target.getAttribute("data-type") != 'allAttraction'){
+         return;
+       }else{
+         e.target.parentNode.setAttribute("class","categoryBtn bg-secondary");
+        let city = selectCity.value;
+        if (city == "選取地區"){
           return;
         }
-         str+=`<li>
-         <div class="attractionList-item">
-           <img class="attractionList-img" src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}">
-           <div class="attractionList-label">
-             <h3>${selectType}</h3>
+        axios.get(
+          `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/${city}?%24top=300&%24format=JSON`,
+          {
+             headers: getAuthorizationHeader()
+          }
+        )
+        .then(function (response) {
+         let cityAttractionData = response.data;
+         let str ="";
+         cityAttractionData.forEach(function(item){
+          if(item.Picture.PictureUrl1 == undefined || item.City == undefined|| item.ScenicSpotName ==undefined){
+            return;
+          }
+           str+=`<li>
+           <div class="attractionList-item">
+             <img class="attractionList-img" src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}">
+             <div class="attractionList-label">
+               <h3>${item.City}</h3>
+             </div>
+             <h4>${item.ScenicSpotName}</h4>
            </div>
-           <h4>${item.ScenicSpotName}</h4>
-         </div>
-       </li>`
-       })
-       document.querySelector(".attractionList").innerHTML = str;
-    }).catch(function (error) {
-      console.log(error);
-     }); 
-  };
+         </li>`
+         })
+         document.querySelector(".attractionList").innerHTML = str;
+        })
+        .catch(function (error) {
+         console.log(error);
+        });       
+
+       }
 })
+
+
+selectGroupMobile.addEventListener('click',function(e){
+  if(e.target.getAttribute("data-type") != 'allAttraction'){
+         return;
+       }else{
+        e.target.parentNode.setAttribute("class","categoryBtn-mobile bg-secondary");
+        let city = selectCity.value;
+        if (city == "選取地區"){
+          return;
+        }
+        axios.get(
+          `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/${city}?%24top=300&%24format=JSON`,
+          {
+             headers: getAuthorizationHeader()
+          }
+        )
+        .then(function (response) {
+         let cityAttractionData = response.data;
+         let str ="";
+         cityAttractionData.forEach(function(item){
+          if(item.Picture.PictureUrl1 == undefined || item.City == undefined|| item.ScenicSpotName ==undefined){
+            return;
+          }
+           str+=`<li>
+           <div class="attractionList-item">
+             <img class="attractionList-img" src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}">
+             <div class="attractionList-label">
+               <h3>${item.City}</h3>
+             </div>
+             <h4>${item.ScenicSpotName}</h4>
+           </div>
+         </li>`
+         })
+         document.querySelector(".attractionList").innerHTML = str;
+        })
+        .catch(function (error) {
+         console.log(error);
+        });       
+
+       }
+})
+
+
+//篩選出有哪些旅遊類別
+// let tourTypes = {};
+// function getTourType(){
+//   axios.get(
+//     `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24format=JSON`,
+//     {
+//       headers: getAuthorizationHeader()
+//     }
+//   ).then(function (response){
+//    const thisData = response.data;
+//    thisData.forEach(function (item){
+//      if(tourTypes[item.Class1] == undefined){
+//        tourTypes[item.Class1]=1;
+//      }else{
+//        tourTypes[item.Class1] +=1;
+//      }
+//    })
+//    console.log(tourTypes);
+//   })
+// }
+//getTourType();
+
+
+//依據選取類別顯示資料(觀光餐飲)
+
+
+// selectGroup.addEventListener('click', function(e){
+//   if(e.target.nodeName != 'A'){
+//     return;
+//   }else{
+//     console.log(e.target.getAttribute("data-type") );
+//     let selectType = e.target.getAttribute("data-type");
+//     axios.get(`https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24filter=contains(Class1,'${selectType}')&%24format=JSON`, {
+//       headers:getAuthorizationHeader()
+//       }
+//     ).then(function(response){
+//       let selectData = response.data;
+//       let str ="";
+     
+//       selectData.forEach(function(item){
+//         if(item.Picture.PictureUrl1 == undefined  || item.Class1 == undefined|| item.ScenicSpotName ==undefined){
+//           return;
+//         }
+//          str+=`<li>
+//          <div class="attractionList-item">
+//            <img class="attractionList-img" src="${item.Picture.PictureUrl1}" alt="${item.Picture.PictureDescription1}">
+//            <div class="attractionList-label">
+//              <h3>${selectType}</h3>
+//            </div>
+//            <h4>${item.ScenicSpotName}</h4>
+//          </div>
+//        </li>`
+//        })
+//        document.querySelector(".attractionList").innerHTML = str;
+//     }).catch(function (error) {
+//       console.log(error);
+//      }); 
+//   };
+// })
 
